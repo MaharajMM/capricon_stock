990b31e321858db60b206a5d9529bc8be722b413
Maharaj M M Behera <101055445+MaharajMM@users.noreply.github.com>
2025-02-22 23:27:04 +0530

    Removal of Unnecessary Color Code

--- lib/const/colors/app_colors.dart
+++ lib/const/colors/app_colors.dart
@@ -4,79 +4,17 @@ class AppColors {
   AppColors._();
   //primary colors
   static const Color appWhite = Color(0xFFFFFFFF);
-  static const Color kPrimaryBgColor = Color(0xFF262320);
-  static const Color kSecondaryBgColor = Color(0xFFFFFFFF);
 
   //alert colors
   static const Color kErrorColor = Colors.red;
   static const Color kSuccessColor = Colors.green;
-  static const Color kSuccessSecondaryColor = Color(0xFFCCFF00);
 
   //ui theme colors
   static const Color kPrimaryThemeColor = Color(0xFFE77E3C);
 
-  //text color
-  static const Color kPrimaryTextColor = Color(0xFFFFFFFF);
-  static const Color kSecondaryTextColor = Color(0xFF4FBA00);
-
-  //buttons color
-  static const Color kPrimaryButtonColor = Color(0xFFE77E3C);
-  static const Color kChangeButtonColor = Color(0xFF313131);
-
-  static const Color kPendingButtonColor = Color(0xFFA34F4F);
-  static const Color kPendingButtonBorderColor = Color(0xFFFF1D00);
-
-  static const Color kConfirmedButtonColor = Color(0xFF3D8129);
-  static const Color kConfirmedButtonBorderColor = Color(0xFF3CFF02);
-
-  static const Color kDownloadButtonColor = Color(0xFF794C30);
-  static const Color kDownloadButtonBorderColor = Color(0xFFE88331);
-
-  static const Color kMakePaymentButtonColor = Color(0xFF2B436A);
-  static const Color kMakePaymentButtonBorderColor = Color(0xFF3B82F6);
-
   //textfield color
   static const Color kTextfieldBgColor = Color(0xFFFCEFEB);
   static const Color kHintTextColor = Color(0xFF999590);
-
-  //grid colors
-  static const Color kGridBgColor = Color(0xFF505050);
-
-  //avatar colors
-  static const Color kDeepAvatarColor = Color(0xFFF28500);
-  static const Color kLightAvatarColor = Color(0xFFF09169);
-
-  //tile colors
-  // static const Color kTileColor = Color(0xFF3D3C3C);
-  static const Color kTileColor = grey900;
-  static const Color kTileIconBgColor = Color(0xFF222121);
-  static const Color kExpandedTileBgColor = Color(0xFF1F1F1F);
-
-  //item suggestion colors
-  static const Color kItemSuggestionColor = Color(0xFFF6ECE6);
-  static const Color kItemSuggestionSelectedColor = Color(0xFFE77E3C);
-
-  //chips color
-  static const Color kChipBgColor = Color(0xFF838383);
-
-  // bottom sheet widget's color
-  static const Color kBottomSheetListColor = Color(0xFF3D3C3C);
-  static const Color kBottomSheetItemCountColor = orange500;
-  static const Color kPerUnitMeasurementColor = Color(0xFF818181);
-  static const Color kItemIncrementColor = Color(0xFFE77E3C);
-  static const Color kItemDecrementColor = Color(0xFF707070);
-  static const Color kBottomSheetDeepButtonColor = Color(0xFFF28500);
-  static const Color kBottomSheetLightButtonColor = Color(0xFFFFF5ED);
-
-  //border color
-  static const Color kBorderColor = Color(0xFF707070);
-
-  //drop shadow colors
-  static Color kDropShadowColor = const Color(0xFF000000).withOpacity(0.7);
-  static Color kSecondaryDropShadowColor = const Color(0xFF000000).withOpacity(0.3);
-
-  //rarely used color
-  static const Color kPrimaryRarelyUsedColor = Color.fromARGB(255, 63, 209, 10);
 
   // Grey color variants
   static const Color grey100 = Color(0xFFf8f8f8);
@@ -101,7 +39,4 @@ class AppColors {
   static const Color orange900 = Color(0xFFFF5100);
 
   static const Color kBlack = Color(0xFF000000);
-  static const darkBackground = Color(0xFF121212);
-  static const darkSecondary = Color(0xFF1E1E1E);
-  static Color yellowColor = const Color.fromARGB(255, 253, 234, 125);
 }
